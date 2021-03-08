fac0d50d8324ba6a46df824ee03f41b72589a54c
Chathura Widanage <chathurawidanage@gmail.com>
2021-03-08 18:38:45 -0500

    handeling response

--- sample/DIDA/App/App.cpp
+++ sample/DIDA/App/App.cpp
@@ -29,58 +29,30 @@ std::string dida_dsp = "dsp";
 std::string dida_mrg = "mrg";
 
 typedef struct _sgx_errlist_t {
-    sgx_status_t err;
-    const char *msg;
-    const char *sug; /* Suggestion */
+  sgx_status_t err;
+  const char *msg;
+  const char *sug; /* Suggestion */
 } sgx_errlist_t;
 
 /* Error code returned by sgx_create_enclave */
 static sgx_errlist_t sgx_errlist[] = {
-    {SGX_ERROR_UNEXPECTED,
-     "Unexpected error occurred.",
-     NULL},
-    {SGX_ERROR_INVALID_PARAMETER,
-     "Invalid parameter.",
-     NULL},
-    {SGX_ERROR_OUT_OF_MEMORY,
-     "Out of memory.",
-     NULL},
-    {SGX_ERROR_ENCLAVE_LOST,
-     "Power transition occurred.",
+    {SGX_ERROR_UNEXPECTED, "Unexpected error occurred.", NULL},
+    {SGX_ERROR_INVALID_PARAMETER, "Invalid parameter.", NULL},
+    {SGX_ERROR_OUT_OF_MEMORY, "Out of memory.", NULL},
+    {SGX_ERROR_ENCLAVE_LOST, "Power transition occurred.",
      "Please refer to the sample \"PowerTransition\" for details."},
-    {SGX_ERROR_INVALID_ENCLAVE,
-     "Invalid enclave image.",
-     NULL},
-    {SGX_ERROR_INVALID_ENCLAVE_ID,
-     "Invalid enclave identification.",
-     NULL},
-    {SGX_ERROR_INVALID_SIGNATURE,
-     "Invalid enclave signature.",
-     NULL},
-    {SGX_ERROR_OUT_OF_EPC,
-     "Out of EPC memory.",
-     NULL},
-    {SGX_ERROR_NO_DEVICE,
-     "Invalid SGX device.",
+    {SGX_ERROR_INVALID_ENCLAVE, "Invalid enclave image.", NULL},
+    {SGX_ERROR_INVALID_ENCLAVE_ID, "Invalid enclave identification.", NULL},
+    {SGX_ERROR_INVALID_SIGNATURE, "Invalid enclave signature.", NULL},
+    {SGX_ERROR_OUT_OF_EPC, "Out of EPC memory.", NULL},
+    {SGX_ERROR_NO_DEVICE, "Invalid SGX device.",
      "Please make sure SGX module is enabled in the BIOS, and install SGX driver afterwards."},
-    {SGX_ERROR_MEMORY_MAP_CONFLICT,
-     "Memory map conflicted.",
-     NULL},
-    {SGX_ERROR_INVALID_METADATA,
-     "Invalid enclave metadata.",
-     NULL},
-    {SGX_ERROR_DEVICE_BUSY,
-     "SGX device was busy.",
-     NULL},
-    {SGX_ERROR_INVALID_VERSION,
-     "Enclave version was invalid.",
-     NULL},
-    {SGX_ERROR_INVALID_ATTRIBUTE,
-     "Enclave was not authorized.",
-     NULL},
-    {SGX_ERROR_ENCLAVE_FILE_ACCESS,
-     "Can't open enclave file.",
-     NULL},
+    {SGX_ERROR_MEMORY_MAP_CONFLICT, "Memory map conflicted.", NULL},
+    {SGX_ERROR_INVALID_METADATA, "Invalid enclave metadata.", NULL},
+    {SGX_ERROR_DEVICE_BUSY, "SGX device was busy.", NULL},
+    {SGX_ERROR_INVALID_VERSION, "Enclave version was invalid.", NULL},
+    {SGX_ERROR_INVALID_ATTRIBUTE, "Enclave was not authorized.", NULL},
+    {SGX_ERROR_ENCLAVE_FILE_ACCESS, "Can't open enclave file.", NULL},
     {SGX_ERROR_NDEBUG_ENCLAVE,
      "The enclave is signed as product enclave, and can not be created as debuggable enclave.",
      NULL},
@@ -88,20 +60,18 @@ static sgx_errlist_t sgx_errlist[] = {
 
 /* Check error conditions for loading enclave */
 void print_error_message(sgx_status_t ret) {
-    size_t idx = 0;
-    size_t ttl = sizeof sgx_errlist / sizeof sgx_errlist[0];
+  size_t idx = 0;
+  size_t ttl = sizeof sgx_errlist / sizeof sgx_errlist[0];
 
-    for (idx = 0; idx < ttl; idx++) {
-        if (ret == sgx_errlist[idx].err) {
-            if (NULL != sgx_errlist[idx].sug)
-                printf("Info: %s\n", sgx_errlist[idx].sug);
-            printf("Error: %s\n", sgx_errlist[idx].msg);
-            break;
-        }
+  for (idx = 0; idx < ttl; idx++) {
+    if (ret == sgx_errlist[idx].err) {
+      if (NULL != sgx_errlist[idx].sug) printf("Info: %s\n", sgx_errlist[idx].sug);
+      printf("Error: %s\n", sgx_errlist[idx].msg);
+      break;
     }
+  }
 
-    if (idx == ttl)
-        printf("Error: Unexpected error occurred.\n");
+  if (idx == ttl) printf("Error: Unexpected error occurred.\n");
 }
 
 /* Initialize the enclave:
@@ -110,169 +80,177 @@ void print_error_message(sgx_status_t ret) {
  *   Step 3: save the launch token if it is updated
  */
 int initialize_enclave(sgx_enclave_id_t *global_eid) {
-    char token_path[MAX_PATH] = {'\0'};
-    sgx_launch_token_t token = {0};
-    sgx_status_t ret = SGX_ERROR_UNEXPECTED;
-    int updated = 0;
+  char token_path[MAX_PATH] = {'\0'};
+  sgx_launch_token_t token = {0};
+  sgx_status_t ret = SGX_ERROR_UNEXPECTED;
+  int updated = 0;
 
-    /* Step 1: try to retrieve the launch token saved by last transaction
+  /* Step 1: try to retrieve the launch token saved by last transaction
    *         if there is no token, then create a new one.
    */
-    /* try to get the token saved in $HOME */
-    const char *home_dir = getpwuid(getuid())->pw_dir;
+  /* try to get the token saved in $HOME */
+  const char *home_dir = getpwuid(getuid())->pw_dir;
 
-    spdlog::info("Looing for the token in {}", home_dir);
+  spdlog::info("Looing for the token in {}", home_dir);
 
-    if (home_dir != NULL &&
-        (strlen(home_dir) + strlen("/") + sizeof(TOKEN_FILENAME) + 1) <= MAX_PATH) {
-        /* compose the token path */
-        strncpy(token_path, home_dir, strlen(home_dir));
-        strncat(token_path, "/", strlen("/"));
-        strncat(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME) + 1);
-    } else {
-        /* if token path is too long or $HOME is NULL */
-        strncpy(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME));
+  if (home_dir != NULL &&
+      (strlen(home_dir) + strlen("/") + sizeof(TOKEN_FILENAME) + 1) <= MAX_PATH) {
+    /* compose the token path */
+    strncpy(token_path, home_dir, strlen(home_dir));
+    strncat(token_path, "/", strlen("/"));
+    strncat(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME) + 1);
+  } else {
+    /* if token path is too long or $HOME is NULL */
+    strncpy(token_path, TOKEN_FILENAME, sizeof(TOKEN_FILENAME));
+  }
+
+  FILE *fp = fopen(token_path, "rb");
+  if (fp == NULL && (fp = fopen(token_path, "wb")) == NULL) {
+    printf("Warning: Failed to create/open the launch token file \"%s\".\n", token_path);
+  }
+
+  if (fp != NULL) {
+    /* read the token from saved file */
+    size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
+    if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
+      /* if token is invalid, clear the buffer */
+      memset(&token, 0x0, sizeof(sgx_launch_token_t));
+      printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
     }
+  }
+  /* Step 2: call sgx_create_enclave to initialize an enclave instance */
+  /* Debug Support: set 2nd parameter to 1 */
+  ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token, &updated, global_eid, NULL);
+  if (ret != SGX_SUCCESS) {
+    print_error_message(ret);
+    if (fp != NULL) fclose(fp);
+    return -1;
+  }
 
-    FILE *fp = fopen(token_path, "rb");
-    if (fp == NULL && (fp = fopen(token_path, "wb")) == NULL) {
-        printf("Warning: Failed to create/open the launch token file \"%s\".\n", token_path);
-    }
-
-    if (fp != NULL) {
-        /* read the token from saved file */
-        size_t read_num = fread(token, 1, sizeof(sgx_launch_token_t), fp);
-        if (read_num != 0 && read_num != sizeof(sgx_launch_token_t)) {
-            /* if token is invalid, clear the buffer */
-            memset(&token, 0x0, sizeof(sgx_launch_token_t));
-            printf("Warning: Invalid launch token read from \"%s\".\n", token_path);
-        }
-    }
-    /* Step 2: call sgx_create_enclave to initialize an enclave instance */
-    /* Debug Support: set 2nd parameter to 1 */
-    ret = sgx_create_enclave(ENCLAVE_FILENAME, SGX_DEBUG_FLAG, &token, &updated, global_eid, NULL);
-    if (ret != SGX_SUCCESS) {
-        print_error_message(ret);
-        if (fp != NULL)
-            fclose(fp);
-        return -1;
-    }
-
-    /* Step 3: save the launch token if it is updated */
-    if (updated == FALSE || fp == NULL) {
-        /* if the token is not updated, or file handler is invalid, do not perform saving */
-        if (fp != NULL)
-            fclose(fp);
-        return 0;
-    }
-
-    /* reopen the file with write capablity */
-    fp = freopen(token_path, "wb", fp);
-    if (fp == NULL)
-        return 0;
-    size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
-    if (write_num != sizeof(sgx_launch_token_t))
-        printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
-    fclose(fp);
+  /* Step 3: save the launch token if it is updated */
+  if (updated == FALSE || fp == NULL) {
+    /* if the token is not updated, or file handler is invalid, do not perform saving */
+    if (fp != NULL) fclose(fp);
     return 0;
+  }
+
+  /* reopen the file with write capablity */
+  fp = freopen(token_path, "wb", fp);
+  if (fp == NULL) return 0;
+  size_t write_num = fwrite(token, 1, sizeof(sgx_launch_token_t), fp);
+  if (write_num != sizeof(sgx_launch_token_t))
+    printf("Warning: Failed to save launch token to \"%s\".\n", token_path);
+  fclose(fp);
+  return 0;
 }
 
 /* OCall functions */
 void ocall_print_string(const char *str) {
-    /* Proxy/Bridge will check the length and null-terminate
+  /* Proxy/Bridge will check the length and null-terminate
    * the input string to prevent buffer overflow.
    */
-    printf("%s", str);
+  printf("%s", str);
 }
 
 std::string destination_dir = "";
 
 void ocall_print_file(const char *str, const char *file, int append) {
-    std::cout << "Writing to file " << std::string(file) << std::endl;
+  std::cout << "Writing to file " << std::string(file) << std::endl;
 
-    std::ofstream stream;
+  std::ofstream stream;
 
-    stream.open(destination_dir + "/" + std::string(file), append == 0 ? std::ofstream::out : std::ofstream::app);
+  stream.open(destination_dir + "/" + std::string(file),
+              append == 0 ? std::ofstream::out : std::ofstream::app);
 
-    if (!stream)
-        std::cout << "Opening file failed" << std::endl;
-    // use operator<< for clarity
-    stream << std::string(str) << std::endl;
-    // test if write was succesful - not *really* necessary
-    if (!stream)
-        std::cout << "Write failed" << std::endl;
+  if (!stream) std::cout << "Opening file failed" << std::endl;
+  // use operator<< for clarity
+  stream << std::string(str) << std::endl;
+  // test if write was succesful - not *really* necessary
+  if (!stream) std::cout << "Write failed" << std::endl;
 
-    stream.close();
+  stream.close();
 }
 
 std::string msg_cmd = tasker::GetCommand(tasker::Commands::MESSAGE);
 
 /* Application entry */
 int SGX_CDECL main(int argc, char *argv[]) {
-    (void)(argc);
-    (void)(argv);
+  (void)(argc);
+  (void)(argv);
 
-    auto start = high_resolution_clock::now();
+  auto start = high_resolution_clock::now();
 
-    /* Global EID shared by multiple threads */
-    sgx_enclave_id_t global_eid = 0;
+  /* Global EID shared by multiple threads */
+  sgx_enclave_id_t global_eid = 0;
 
-    /* Initialize the enclave */
-    spdlog::info("Initializing enclave....");
-    if (initialize_enclave(&global_eid) < 0) {
-        return -1;
-    }
+  /* Initialize the enclave */
+  spdlog::info("Initializing enclave....");
+  if (initialize_enclave(&global_eid) < 0) {
+    return -1;
+  }
 
-    auto stop = high_resolution_clock::now();
-    auto duration = duration_cast<milliseconds>(stop - start);
+  auto stop = high_resolution_clock::now();
+  auto duration = duration_cast<milliseconds>(stop - start);
 
-    printf("Enclave initialized in %ld ms\n", duration.count());
+  printf("Enclave initialized in %ld ms\n", duration.count());
 
-    int32_t validation_code;
-    std::string validation_msg;
+  int32_t validation_code;
+  std::string validation_msg;
 
-    tasker::Worker worker(gen_random(16), TYPE_SECURE);
-    worker.OnMessage([&worker, &argc, &argv, &global_eid, &validation_code, &validation_msg](std::string msg) {
+  tasker::Worker worker(gen_random(16), TYPE_SECURE);
+  worker.OnMessage(
+      [&worker, &argc, &argv, &global_eid, &validation_code, &validation_msg](std::string msg) {
         spdlog::info("Handeling command {}", msg);
         // determining the DIDA command
         std::string dida_command = msg.substr(0, 3);
 
         if (dida_command.compare(dida_dsp) == 0) {
-            auto dispatch_command = DispatchCommand(msg);
-            dispatch_command.Parse(&validation_code, &validation_msg);
+          auto dispatch_command = DispatchCommand(msg);
+          dispatch_command.Parse(&validation_code, &validation_msg);
 
-            printf("Command parsed...");
-            if (validation_code == 0) {
-                printf("Validation passed!");
-                destination_dir = dispatch_command.GetDestination();
-                dsp(dispatch_command, global_eid);
-                destination_dir = "";
-                worker.Send(msg_cmd, create_response(0, ""));
-            } else {
-                printf("Validation failed %s", validation_msg.c_str());
-                worker.Send(msg_cmd, validation_msg);
-            }
+          printf("Command parsed...");
+          if (validation_code == 0) {
+            printf("Validation passed!");
+            destination_dir = dispatch_command.GetDestination();
+            dsp(dispatch_command, global_eid);
+            destination_dir = "";
+            worker.Send(msg_cmd, create_response(0, ""));
+          } else {
+            printf("Validation failed %s", validation_msg.c_str());
+            worker.Send(msg_cmd, validation_msg);
+          }
         } else if (dida_command.compare(dida_mrg) == 0) {
-            auto mrg_command = MergeCommand(msg);
+          auto mrg_command = MergeCommand(msg);
+          mrg_command.Parse(&validation_code, &validation_msg);
+          printf("Merge Command parsed...");
+          if (validation_code == 0) {
+            destination_dir = mrg_command.GetResultsDir();
+            printf("Validation passed!");
             mrg(mrg_command, global_eid);
+            destination_dir = "";
+            worker.Send(msg_cmd, create_response(0, ""));
+          } else {
+            printf("Validation failed %s", validation_msg.c_str());
+            worker.Send(msg_cmd, validation_msg);
+          }
         } else {
-            //unknown command
-            printf("Unknown command %s", dida_command.c_str());
+          // unknown command
+          printf("Unknown command %s", dida_command.c_str());
         }
-    });
+      });
 
-    std::string server_url = "tcp://localhost:5050";
-    if (argc == 2) {
-        server_url = argv[1];
-    }
-    worker.Start(server_url);
+  std::string server_url = "tcp://localhost:5050";
+  if (argc == 2) {
+    server_url = argv[1];
+  }
+  worker.Start(server_url);
 
-    /* Destroy the enclave */
-    start = high_resolution_clock::now();
-    sgx_destroy_enclave(global_eid);
-    stop = high_resolution_clock::now();
-    duration = duration_cast<milliseconds>(stop - start);
-    printf("Info: Enclave Destroyed in %ld ms.\n", duration.count());
+  /* Destroy the enclave */
+  start = high_resolution_clock::now();
+  sgx_destroy_enclave(global_eid);
+  stop = high_resolution_clock::now();
+  duration = duration_cast<milliseconds>(stop - start);
+  printf("Info: Enclave Destroyed in %ld ms.\n", duration.count());
 
-    return 0;
+  return 0;
 }
